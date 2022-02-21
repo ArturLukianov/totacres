39a47c739df33c4d57bbe87b3f0e5885982c187f
Magnum Malum <no@mail>
2022-02-21 12:52:33 +0300

    basic file structure

---
+++ nodes/node.py
@@ -0,0 +1,3 @@
+class Node:
+    name = "Node"
+    content = None
---
+++ transformators/transformator.py
@@ -0,0 +1,8 @@
+class Transformator:
+    name = "Transformator"
+    input_node = "Node"
+    output_node = "Node"
+
+    @classmethod
+    def transform(input_node):
+        return None
